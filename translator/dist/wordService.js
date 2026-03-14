b0f30e76a9c8750a329b33523071ab3883811f22
sbelousov <sbelousov@dataart.com>
2026-03-14 14:21:03 +0400

    Stop tracking dist build output

--- translator/dist/wordService.js
+++
@@ -1,9 +0,0 @@
-export function makeNewWord(value, language) {
-    value = value.trim();
-    return {
-        id: crypto.randomUUID(),
-        value,
-        language
-    };
-}
-//# sourceMappingURL=wordService.js.map
